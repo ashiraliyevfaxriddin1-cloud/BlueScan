6f5b9f0ad3d8187736673f5f52dd76cb83c88234
ashiraliyevfaxriddin1-cloud <ashiraliyevfaxriddin1@gmail.com>
2026-07-22 16:25:55 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+main.py
+from kivy.app import App
+from kivy.uix.label import Label
+
+class BlueScanApp(App):
+    def build(self):
+        return Label(
+            text="BlueScan\nBluetooth Scanner",
+            font_size=30
+        )
+
+BlueScanApp().run()
